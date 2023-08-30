18733a40ec87bdd6332d2777732a1000035326f4
ikemba3 <emmatheduke@gmail.com>
2023-08-30 22:13:01 +0100

    commented jquery prevent default issue

--- js/jquery.scroll.js
+++ js/jquery.scroll.js
@@ -1,3 +1,9 @@
-$('a').click(function(e){
-    e.preventDefault();
-})
+// $(document).ready(function(){
+//     var hash = window.location.hash;
+//     var link = $('a');
+//     $('a').click(function(e){
+//         e.preventDefault();
+//         hash = link.attr("href");
+//         window.location = hash;
+//     })
+// })
