6c8960f077acbbae74b2738813bb6d536d68dc17
MacBook Pro <macbookpro@MacBooks-MacBook-Pro.local>
2024-04-12 15:03:53 -0700

    123

--- js/jquery.scroll.js
+++ js/jquery.scroll.js
@@ -1,9 +1,9 @@
-// $(document).ready(function(){
-//     var hash = window.location.hash;
-//     var link = $('a');
-//     $('a').click(function(e){
-//         e.preventDefault();
-//         hash = link.attr("href");
-//         window.location = hash;
-//     })
-// })
+$(document).ready(function(){
+    var hash = window.location.hash;
+    var link = $('a');
+    $('a').click(function(e){
+        e.preventDefault();
+        hash = link.attr("href");
+        window.location = hash;
+    })
+})
